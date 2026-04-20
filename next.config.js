9d04bfa923e5634158e118bf1f3a52a11df7d5d3
jplabdiamond-jpg <jp.lab.diamond@gmail.com>
2026-04-20 17:19:50 +0900

    fix: add webpack alias for @ path resolution on Linux/Render

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["jjfddcngrewyxfycffrg.supabase.co"],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
   },
 };
 
